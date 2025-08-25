4d6ef3207c216d6ca007f1e93a91b84c0579e065
sophie nikol <nikolenko.sofiya@list.ru>
2025-08-25 19:37:50 +0200

    Add training baseline model

--- scripts/dataset_loader.py
+++
@@ -1,8 +0,0 @@
-import os
-import requests
-
-input_file_path = os.path.join('data/dataset.txt')
-if not os.path.exists(input_file_path):
-    data_url = 'https://raw.githubusercontent.com/karpathy/char-rnn/master/data/tinyshakespeare/input.txt'
-    with open(input_file_path, 'w', encoding='utf-8') as f:
-        f.write(requests.get(data_url).text)
